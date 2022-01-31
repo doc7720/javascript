ce3d8ec2b046a5d0bd6b1af05cb896bced6e8016
doc7720 <bodyazastavnyi97@gmail.com>
2022-02-01 01:02:45 +0200

    add homework lesson 1

---
+++ lesson1/homework/main.js
@@ -0,0 +1,59 @@
+// let a = `hello`, b = `owu`, c = `com`, d = `ua`, e = 1, f = 10, g = -999, h = 123;
+// const PI = 3.14;
+// let j = 2.7, k = 16, l = true, m = false;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+//
+// alert(PI);
+//
+// document.write(`<div>` + j + `</div>`);
+// document.write(`<div>` + k + `</div>`);
+// document.write(`<div>` + l + `</div>`);
+// document.write(`<div>` + m + `</div>`);
+
+// a = `world`;
+// b = `tesla`;
+// c = `ru`;
+// d = `car`;
+// e = 3;
+// f = 15;
+// g = -765;
+// h = 456;
+// j = 5.6;
+// k = 44;
+// l = false;
+// m = true;
+//
+//
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+// console.log(g);
+// console.log(h);
+//
+// alert(PI);
+//
+// document.write(`<div>` + j + `</div>`);
+// document.write(`<div>` + k + `</div>`);
+// document.write(`<div>` + l + `</div>`);
+// document.write(`<div>` + m + `</div>`);
+//
+// let firstName = `Bohdan `;
+// let middleName = `Tarasovich`;
+// let lastName = `Zastavnyi`;
+// let myAge = 24;
+// let fullName = firstName + middleName + lastName;
+// let name = prompt('Вітаю ', firstName + middleName);
+// let age = prompt("Тобі ", myAge );
+// console.log("Вітаю " + name + ". Тобі " + age + " років.");
+
