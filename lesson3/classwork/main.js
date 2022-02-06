fd43afd1bda0a4c44c62341ac3603234b5fe0405
doc7720 <bodyazastavnyi97@gmail.com>
2022-02-06 14:14:26 +0200

    add classwork lesson 3

---
+++ lesson3/classwork/main.js
@@ -0,0 +1,139 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+
+let numberArray = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+
+// 1. перебрати його циклом while
+
+// i = 0;
+// while (i < numberArray.length) {
+//     console.log(numberArray[i]);
+//     i++;
+// }
+
+// 2. перебрати його циклом for
+
+// for (let j = 0; j < numberArray.length; j++) {
+//     console.log(numberArray[j]);
+// }
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом
+
+// i = 0;
+// while (i < numberArray.length) {
+//     if (i % 2 !== 0) {
+//         console.log(numberArray[i]);
+//     }
+//     i++;
+// }
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом
+
+// for (let i = 0; i < numberArray.length; i++) {
+//     if (i % 2 !== 0) {
+//         console.log(numberArray[i]);
+//     }
+// }
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення
+
+// i = 0;
+// while (i < numberArray.length) {
+//     if (i % 2 === 0) {
+//         console.log(numberArray[i]);
+//     }
+//     i++;
+// }
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення
+
+// for (let i = 0; i < numberArray.length; i++) {
+//     if (i % 2 === 0) {
+//         console.log(numberArray[i]);
+//     }
+// }
+
+// 7. замінити кожне число кратне 3 на слово "okten"
+
+// for (let i = 0; i < numberArray.length; i++) {
+//     if (numberArray[i] % 3 === 0) {
+//     numberArray[i] = 'okten';
+//     console.log(numberArray[i]);
+//     }
+// }
+// console.log(numberArray);
+
+// 8. вивести масив в зворотньому порядку.
+
+// let numberArrayBack = [2, 17, 13, 6, 22, 31, 45, 66, 100, -18];
+// for (let i = numberArrayBack.length - 1; i >= 0; i--) {
+//     console.log(numberArrayBack[i]);
+// }
+
+// 9. всі попередні завдання (окрім 8), але в зворотньому циклі (с заду на перед)
+
+// 7. замінити кожне число кратне 3 на слово "okten" (звородній)
+
+// for (let i = numberArray.length - 1; i >= 0; i--) {
+//     if (numberArray[i] % 3 === 0) {
+//         numberArray[i] = 'okten';
+//     }
+//     console.log(numberArray[i]);
+// }
+
+
+// 6. перебрати циклом for та вивести  числа тільки парні  значення (звородній)
+
+// for (let i = numberArray.length - 1; i >= 0; i--) {
+//     if (i % 2 === 0) {
+//         console.log(numberArray[i]);
+//     }
+// }
+
+
+// 5. перебрати циклом while та вивести  числа тільки парні  значення (звородній)
+
+// i = 0;
+// while (i > numberArray.length - 1) {
+//     if (i % 2 === 0) {
+//         console.log(numberArray[i]);
+//     }
+//     i--;
+// }
+
+
+// 4. перебрати циклом for та вивести  числа тільки з непарним індексом  (звородній)
+
+// for (let i = numberArray.length - 1; i >= 0; i--) {
+//     if (i % 2 !== 0) {
+//         console.log(numberArray[i]);
+//     }
+// }
+
+
+// 3. перебрати циклом while та вивести  числа тільки з непарним індексом   (звородній)
+
+// i = 0;
+// while (i < numberArray.length) {
+//     if (i % 2 !== 0) {
+//         console.log(numberArray[i]);
+//     }
+//     i++;
+// }
+
+
+// 2. перебрати його циклом for
+
+// for (let i = numberArray.length - 1; i >= 0; i--) {
+//     console.log(numberArray[j]);
+
+
+
+// 1. перебрати його циклом while
+
+// i = 0;
+// while (i < numberArray.length) {
+//     console.log(numberArray[i]);
+//     i++;
+// }
+
+
