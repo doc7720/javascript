f694a48f7befce7368ab62ec13282bb75af2a940
doc7720 <bodyazastavnyi97@gmail.com>
2022-02-07 23:40:53 +0200

    add classwork lesson 4

---
+++ lesson4/classwork/main.js
@@ -0,0 +1,79 @@
+// - створити функцію яка приймає три числа та виводить найменьше. (Без Math.min!)
+
+
+// function numberMinimum(number1, number2, number3) {
+//     if (number1 < number2 && number1 < number3) {
+//         console.log(number1);
+//     } else if (number2 < number1 && number2 < number3) {
+//         console.log(number2);
+//     } else {
+//         console.log(number3);
+//     }
+// }
+// numberMinimum(10, 25, 5);
+
+
+// - створити функцію яка приймає три числа та виводить найбільше. (Без Math.max!)
+
+
+// function numberMaximum(number1, number2, number3) {
+//     if (number1 > number2 && number1 > number3) {
+//         console.log(number1);
+//     } else if (number2 > number1 && number2 > number3) {
+//         console.log(number2);
+//     } else {
+//         console.log(number3);
+//     }
+// }
+// numberMaximum(10, 25, 5);
+
+
+// - створити функцію яка повертає найбільше число з масиву
+
+
+// let arrayMaximum = [12, 45, 216, 1, 9494, 121291, 562, 121, 3151, 111];
+// function maxArray(array) {
+//     let maximum = array[0];
+//     for (const arrayElement of array) {
+//         if (arrayElement > maximum) {
+//             maximum = arrayElement;
+//         }
+//     }
+//     return maximum;
+// }
+// document.write(maxArray(arrayMaximum));
+
+
+// - створити функцію яка повертає найменьше число з масиву
+
+
+// let arrayMinimum = [12, 45, 216, 1, 9494, 121291, 562, 121, 3151, 111];
+// function minArray(array) {
+//     let minimum = array[0];
+//     for (const arrayElement of array) {
+//         if (arrayElement < minimum) {
+//             minimum = arrayElement;
+//         }
+//     }
+//     return minimum;
+// }
+// document.write(minArray(arrayMinimum));
+
+
+// - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
+
+
+// - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
+
+
+// - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+
+
+// - створити функцію яка заповнює масив рандомними числами
+// (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
+
+
+// - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+
+// - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
