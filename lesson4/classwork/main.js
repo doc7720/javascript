a19de0140d4def25c176bc64336f5bbc36b41163
doc7720 <bodyazastavnyi97@gmail.com>
2022-02-08 13:59:24 +0200

    add classwork lesson 4 (2)

--- lesson4/classwork/main.js
+++ lesson4/classwork/main.js
@@ -63,17 +63,57 @@
 // - створити функцію яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад [1,2,10]->13
 
 
+// let arraySum = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// function SumElements (array) {
+//     let sum = 0;
+//     for (const arrayElement of array) {
+//     sum = arrayElement;
+//     sum = arrayElement + sum;
+//     }
+//     return sum;
+// }
+// document.write(SumElements(arraySum));
+
+
 // - створити функцію яка приймає масив чисел та повертає середнє арифметичне його значень.
 
 
+// let arrayMedium = [12, 28, 20];
+// function mediumElements(array) {
+//     let sum = 0;
+//     for (const arrayElement of array) {
+//         sum = arrayElement;
+//         sum = arrayElement + sum;
+//     }
+//     return sum/array.length;
+// }
+// document.write(mediumElements(arrayMedium));
+
+
 // - створити функцію яка приймає будь-яку кількість чисел, повертає найменьше, а виводить найбільше (Math використовувати заборонено);
+
+
+
 
 
 // - створити функцію яка заповнює масив рандомними числами
 // (цей код генерує рандомні числа в діапазоні від 0 до 100 - Math.round(Math.random()*100)) та виводить його.
 
 
+// function random(array) {
+//     let arrayRandom = [];
+//     for (let i = 0; i < array; i++) {
+//     arrayRandom.push(Math.floor(Math.random()*100));
+//     }
+//     return arrayRandom;
+// }
+// document.write(random(10));
+
+
 // - створити функцію яка заповнює масив рандомними числами в діапазоні від 0 до limit. limit - аргумент, який характеризує кінцеве значення діапазону.
+
+
+
 
 
 // - Функція приймає масив та робить з нього новий масив в зворотньому порядку. [1,2,3] -> [3, 2, 1].
